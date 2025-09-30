28184517bf9d3705d2c9797762a60e9e6dbfce51
pekkao <pekka.ojala@oamk.fi>
2025-09-30 23:43:41 +0300

    Changed texts and port enviroment variable

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const server = require('./server');
 
 // App Service on Windows: PORT
 // App Service on Linux: WEB_PORT
-const port = process.env.PORT || 3000;
+const port = process.env.WEB_PORT || 3000;
 const hostname = '0.0.0.0';
 
 server.create()
     .then(app => {
         app.listen(port, hostname, () => {
-            console.log(`Server has started on port ${port}!`);
+            console.log(`Express server has started on port ${port}!`);
         });
     }).catch(err => console.log(err));
